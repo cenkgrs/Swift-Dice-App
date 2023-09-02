58dda100cc0a0674c6d7fd0c27d5eae22e0dac3d
cenkgrs <cenkgrs@gmail.com>
2023-09-02 14:42:42 +0300

    - change image programatically

--- Swift-Dice-App/ViewController.swift
+++ Swift-Dice-App/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+            
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix");
     }
 
 
